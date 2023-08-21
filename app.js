cecddb778de3d6ca47002f08715e2fb7439c7a22
Cian Byrne <94652532+cianbyrne00@users.noreply.github.com>
2023-08-21 22:47:42 +0100

    Javascript fix on body position

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav_list');
+    const body = document.querySelector('.body');
     const navLinks = document.querySelectorAll('.nav_list li');
    
     //toggle nav
     burger.addEventListener('click', ()=>{
+    body.classList.toggle('fixed');
     nav.classList.toggle('nav-active');
     //burger animation 
     burger.classList.toggle('toggle');
